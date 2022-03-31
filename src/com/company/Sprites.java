e344880e96af9d07005ac12759f59f7faad8ec69
Jaanai Martinez <99719430+jaanaip@users.noreply.github.com>
2022-03-31 14:47:43 -0600

    Update Sprites.java

--- src/com/company/Sprites.java
+++ src/com/company/Sprites.java
@@ -14,45 +14,57 @@ public class Sprites {
         ArrayList<Punto> cactus;
         Punto dos = new Punto();
         Punto uno = new Punto();
-
-        uno.setXY(coordX +  25,coordY + 27);
-        cactus=graficador.pintaCirculo(uno, 5);
-        uno.setXY(coordX + 8,coordY + 33);
+        
+        //Circulo Principal
+        uno.setXY(coordX +  25,coordY + 32);
+        cactus=graficador.pintaCirculo(uno,5);
+        //Circulo 1 (Brazo iz)
+        uno.setXY(coordX + 8,coordY + 38);
         cactus.removeAll(graficador.pintaCirculo(uno,3));
         cactus.addAll(graficador.pintaCirculo(uno,3));
-        uno.setXY(coordX + 8,coordY + 54);
+        //Circulo 2 (Brazo iz)
+        uno.setXY(coordX + 8,coordY + 59);
         cactus.removeAll(graficador.pintaCirculo(uno,4));
         cactus.addAll(graficador.pintaCirculo(uno,4));
-        uno.setXY(coordX + 41,coordY + 57);
-        cactus.removeAll(graficador.pintaCirculo(uno,4));
-        cactus.addAll(graficador.pintaCirculo(uno,4));
-        uno.setXY(coordX + 42,coordY + 38);
+        //Circulo 1 (Brazo de)
+        uno.setXY(coordX + 42,coordY + 43);
         cactus.removeAll(graficador.pintaCirculo(uno,3));
         cactus.addAll(graficador.pintaCirculo(uno,3));
-        uno.setXY(coordX + 5,coordY + 35);
-        dos.setXY(coordX + 5,coordY + 51);
+        //Circulo 2 (Brazo de)
+        uno.setXY(coordX + 42,coordY + 63);
+        cactus.removeAll(graficador.pintaCirculo(uno,4));
+        cactus.addAll(graficador.pintaCirculo(uno,4));    
+        //Rectangulo vertical izquierdo (Brazo Cactus)
+        uno.setXY(coordX + 5,coordY + 40);
+        dos.setXY(coordX + 5,coordY + 56);
         cactus.removeAll(graficador.pintaRectangulo(uno,dos,7));
-        cactus.addAll(graficador.pintaRectangulo(uno,dos,7));
-        uno.setXY(coordX + 11,coordY + 51);
-        dos.setXY(coordX + 11,coordY + 58);
+        cactus.addAll(graficador.pintaRectangulo(uno,dos,7)); 
+        //Rectangulo horizontal izquierdo (Brazo Cactus)
+        uno.setXY(coordX + 11,coordY + 56);
+        dos.setXY(coordX + 11,coordY + 62);
         cactus.removeAll(graficador.pintaRectangulo(uno,dos,9));
         cactus.addAll(graficador.pintaRectangulo(uno,dos,9));
-        uno.setXY(coordX + 20,coordY + 30);
-        dos.setXY(coordX + 20,coordY + 94);
+        //Rectangulo Principal (Cuerpo Cactus)
+        uno.setXY(coordX + 20,coordY + 35);
+        dos.setXY(coordX + 20,coordY + 99);
         cactus.removeAll(graficador.pintaRectangulo(uno,dos,11));
         cactus.addAll(graficador.pintaRectangulo(uno,dos,11));
-        uno.setXY(coordX + 31,coordY + 55);
-        dos.setXY(coordX + 31,coordY + 61);
+         //Rectangulo horizontal derecho (Brazo Cactus)
+        uno.setXY(coordX + 31,coordY + 60);
+        dos.setXY(coordX + 31,coordY + 66);
         cactus.removeAll(graficador.pintaRectangulo(dos,uno,9));
         cactus.addAll(graficador.pintaRectangulo(dos,uno,9));
-        uno.setXY(coordX + 39,coordY + 40);
-        dos.setXY(coordX + 39,coordY + 55);
+        //Rectangulo vertical izquierdo (Brazo Cactus)
+        uno.setXY(coordX + 39,coordY + 45);
+        dos.setXY(coordX + 39,coordY + 59);
         cactus.removeAll(graficador.pintaRectangulo(uno,dos,7));
         cactus.addAll(graficador.pintaRectangulo(uno,dos,7));
+        
         return cactus;
     }
 
     static ArrayList<Punto> getCactus2(){
+        //Cactus de a tres xd
         ArrayList<Punto> cactus;
         Punto dos = new Punto();
         Punto uno = new Punto();
@@ -70,47 +82,56 @@ public class Sprites {
         uno.setXY(coordX + 58,coordY + 11);
         dos.setXY(coordX + 58,coordY + 35);
         dino=graficador.pintaRectangulo(uno,dos,38); //rectangulo cabeza
+        
         uno.setXY(coordX + 58,coordY + 36);
         dos.setXY(coordX + 58,coordY + 50);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,15)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,15)); //rectangulo cuello
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,16)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,16)); //rectangulo cuello
+        
         uno.setXY(coordX + 27,coordY + 51);
         dos.setXY(coordX + 27,coordY + 82);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,43)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,43)); //rectangulo cuerpo
-        uno.setXY(coordX + 30,coordY + 83);
-        dos.setXY(coordX + 30,coordY + 98);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //rectangulo pierna iz
-        uno.setXY(coordX + 50,coordY + 83);
-        dos.setXY(coordX + 50,coordY + 99);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,47)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,47)); //rectangulo cuerpo
+        
+        uno.setXY(coordX + 33,coordY + 83);
+        dos.setXY(coordX + 33,coordY + 99);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,11)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,11)); //rectangulo pierna de
+        dino.addAll(graficador.pintaRectangulo(uno,dos,11)); //rectangulo pierna iz
+        
+        uno.setXY(coordX + 47,coordY + 83);
+        dos.setXY(coordX + 47,coordY + 98);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //rectangulo pierna de
+        
         uno.setXY(coordX + 49,coordY + 42);
         dos.setXY(coordX + 49,coordY + 50);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,9)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,9)); //lomoxd
-        uno.setXY(coordX + 70,coordY + 50);
-        dos.setXY(coordX + 70,coordY + 54);
+        
+        uno.setXY(coordX + 74,coordY + 50);
+        dos.setXY(coordX + 74,coordY + 54);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,9)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,9)); //brazo
+        
         uno.setXY(coordX + 5,coordY + 42);
         dos.setXY(coordX + 5,coordY + 66);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //cola1
+        
         uno.setXY(coordX + 9,coordY + 51);
         dos.setXY(coordX + 9,coordY + 73);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,10)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,10)); //cola2
+        
         uno.setXY(coordX + 17,coordY + 58);
         dos.setXY(coordX + 17,coordY + 79);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,10)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,10)); //cola3
 
         //OJO
-        uno.setXY(coordX + 63,coordY + 15);
-        dos.setXY(coordX + 63,coordY + 21);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,7)); //Elimina repetidos
+        uno.setXY(coordX + 65,coordY + 16);
+        dos.setXY(coordX + 65,coordY + 23);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
 
         return dino;
     }
@@ -123,47 +144,56 @@ public class Sprites {
         uno.setXY(x+58,y+11);
         dos.setXY(x+58,y+35);
         dino=graficador.pintaRectangulo(uno,dos,38); //rectangulo cabeza
+        
         uno.setXY(x+58,y+36);
         dos.setXY(x+58,y+50);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,15)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,15)); //rectangulo cuello
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,16)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,16)); //rectangulo cuello
+        
         uno.setXY(x+27,y+51);
         dos.setXY(x+27,y+82);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,43)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,43)); //rectangulo cuerpo
-        uno.setXY(x+34,y+83);
-        dos.setXY(x+34,y+99);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,11)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,11)); //rectangulo pierna iz
-        uno.setXY(x+48,y+83);
-        dos.setXY(x+48,y+98);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,47)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,47)); //rectangulo cuerpo
+        
+        uno.setXY(x+30,y+83);
+        dos.setXY(x+30,y+98);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
-        dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //rectangulo pierna de
+        dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //rectangulo pierna iz
+        
+        uno.setXY(x+50,y+83);
+        dos.setXY(x+50,y+99);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,11)); //Elimina repetidos
+        dino.addAll(graficador.pintaRectangulo(uno,dos,11)); //rectangulo pierna de
+        
         uno.setXY(x+49,y+42);
         dos.setXY(x+49,y+50);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,9)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,9)); //lomoxd
-        uno.setXY(x+70,y+50);
-        dos.setXY(x+70,y+54);
+        
+        uno.setXY(x+74,y+50);
+        dos.setXY(x+74,y+54);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,9)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,9)); //brazo
-        uno.setXY(x+5,y+40);
+        
+        uno.setXY(x+5,y+42);
         dos.setXY(x+5,y+66);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,8)); //cola1
+        
         uno.setXY(x+9,y+51);
         dos.setXY(x+9,y+73);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,10)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,10)); //cola2
+        
         uno.setXY(x+17,y+58);
         dos.setXY(x+17,y+79);
         dino.removeAll(graficador.pintaRectangulo(uno,dos,10)); //Elimina repetidos
         dino.addAll(graficador.pintaRectangulo(uno,dos,10)); //cola3
 
         //OJO
-        uno.setXY(x+63,y+15);
-        dos.setXY(x+63,y+21);
-        dino.removeAll(graficador.pintaRectangulo(uno,dos,7)); //Elimina repetidos
+        uno.setXY(x+65,y+16);
+        dos.setXY(x+65,y+23);
+        dino.removeAll(graficador.pintaRectangulo(uno,dos,8)); //Elimina repetidos
 
         return dino;
     }
